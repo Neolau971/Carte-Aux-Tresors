abe60def3cc88fb9e3aede2e94a1e76560507f56
neola <ulrick.neola@hotmail.fr>
2020-11-20 21:55:53 +0100

    #0003 gestion tresos

--- src/gestionTresors.js
+++ src/gestionTresors.js
@@ -1,3 +1,32 @@
-module.exports = function GestionTresors() {
+module.exports = {
 
+    updateTresors : function(axeHAventurier, axeVAventurier, tresors, directive) {
+        let tresorsinfos = []
+        tresorsinfos = tresors
+    
+        if (directive !== "D" && directive !== "G") {
+            tresorsinfos.forEach(tresor => {
+                (tresor.axeH === axeHAventurier && tresor.axeV === axeVAventurier) ? ( tresor.nombre = tresor.nombre -1 ) : ("")
+            })
+        } 
+    
+        return tresorsinfos
+
+    },
+
+    tresorsOfAventurier : function (tresorInitial, tresors) {
+        let tresorsFound = 0
+        let tresorInitState = []
+        let tresorsFinalState = []
+
+        tresorInitState = tresorInitial
+        tresorsFinalState = tresors
+
+        for (let i = 0; i < tresorInitState.length; i++) {
+            tresorsFound = tresorsFound + (tresorInitState[i].nombre - tresorsFinalState[i].nombre)
+        }
+
+        return tresorsFound
+    
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 const AventurierNewLocalisation = require("../src/aventurierNewLocalisation")
+const GestionTresors = require("../src/gestionTresors")
 
 module.exports = function Main() {
 
-    let dimCarte = [3,4]
     let positionMontagne = [{axeH : 1, axeV : 0}, {axeH : 2, axeV : 1}]
-    let Tresor = [{axeH : 0, axeV : 3, nombre: 2}, {axeH : 1, axeV : 3, nombre: 3}]
+    let tresors = [{axeH : 0, axeV : 3, nombre: 2}, {axeH : 1, axeV : 3, nombre: 3}]
+    let tresorInitial = [{axeH : 0, axeV : 3, nombre: 2}, {axeH : 1, axeV : 3, nombre: 3}]
     let aventuriers = [{nom : "Lara", axeH : 1, axeV : 1, lastOrientation : "S", sequence : "AADADAGGA"}]
 
     let currentAventurier = {nom : "Lara", axeH : 1, axeV : 1, lastOrientation : "S", sequence : "AADADAGGA"}
@@ -15,7 +16,11 @@ module.exports = function Main() {
             currentAventurier.axeH = aventurierLocalisation[0]
             currentAventurier.axeV = aventurierLocalisation[1]
             currentAventurier.lastOrientation = aventurierLocalisation[2]
-            console.log(currentAventurier)
 
-        } 
+            tresors = GestionTresors.updateTresors(currentAventurier.axeH, currentAventurier.axeV, tresors, currentAventurier.sequence[i])
+        }
+
+        let tresorFinal = GestionTresors.tresorsOfAventurier(tresorInitial, tresors)
+        
+        console.log(tresors, currentAventurier, tresorFinal)
 } 
